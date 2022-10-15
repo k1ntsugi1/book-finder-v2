05d4c756c4b726d92d049004d906e8c4fcafc9f3
k1ntsugi1 <bmasalimov5@gmail.com>
2022-10-16 00:55:00 +0300

    styling #3

--- src/components/sidebar/brush/BrushSection.tsx
+++ src/components/sidebar/brush/BrushSection.tsx
@@ -12,11 +12,22 @@ import GlassElement from '../../GlassElement';
 
 import saveOptionsOfStyleHandler from '../../../helpersFunc/saveOptionsOfStyleHandler'
 
+interface IResponseData {
+    data: {
+        url: string,
+    }
+}
+
 interface BrushProps {
     showStateOfBrush: string,
 };
 
 export interface IInitialStateImmer {
+    stateOfBackground: string,
+    images: {
+        activeImage: string,
+        bodyImages: string[]
+    }
     colors: {
         bodyColor: string,
         cardColor: string,
@@ -24,8 +35,6 @@ export interface IInitialStateImmer {
         textColor: string,
         scrollLoaderColor: string,
     },
-    stateOfBackground: string,
-    backgroundImages: string[],
 };
 
 interface ActionOfReducerImmer {
@@ -33,6 +42,11 @@ interface ActionOfReducerImmer {
     value?: string | boolean
 };
 const initialState: IInitialStateImmer = {
+    stateOfBackground: localStorage.getItem('stateOfBackground') ?? 'image',
+    images: {
+        activeImage: localStorage.getItem('activeImage') ?? DefaultImageBackground,
+        bodyImages: JSON.parse(localStorage.getItem('backgroundImages') ?? JSON.stringify({ active: 0, images: [DefaultImageBackground] }))
+    },
     colors: {
         bodyColor: localStorage.getItem('bodyColor') ?? '#78a1a1',
         cardColor: localStorage.getItem('cardColor') ?? '#C0C3E6',
@@ -40,8 +54,6 @@ const initialState: IInitialStateImmer = {
         textColor: localStorage.getItem('textColor') ?? '#FAFFFF',
         scrollLoaderColor: localStorage.getItem('scrollLoaderColor') ?? '#78a1a1',
     },
-    stateOfBackground: 'image',
-    backgroundImages: JSON.parse(localStorage.getItem('backgroundImages') ?? JSON.stringify([DefaultImageBackground])),
 }
 
 // что-то много as string подумай как исправить
@@ -68,14 +80,19 @@ function reducer(draft: IInitialStateImmer, action: ActionOfReducerImmer) {
         case "updateStateOfBackground":
             draft.stateOfBackground = action.value as string;
             return void draft;
-        case "updateStatebackgroundOfBody":
-            return void draft.backgroundImages.push(action.value as string);
+        case "updateActiveImage":
+            draft.images.activeImage = action.value as string;
+            return void draft;
+        case "updateBodyImages":
+            draft.images.bodyImages.push(action.value as string);
+            draft.images.activeImage = action.value as string;
+            return void draft;
     }
 }
 
 
 const BrushSection: React.FC<BrushProps> = (props) => {
-    const ref = useRef(null)
+    const uploadFileRef = useRef<HTMLInputElement>(null)
     const { showStateOfBrush } = props;
     const [state, dispatchImmer] = useImmerReducer(reducer, initialState);
 
@@ -94,16 +111,27 @@ const BrushSection: React.FC<BrushProps> = (props) => {
     const classnamesOfRotateCard = cn('border-bottom')
 
     useEffect(() => {
+        document.documentElement.style.setProperty('--color-body', state.colors.bodyColor);
         document.documentElement.style.setProperty('--color-card', state.colors.cardColor);
         document.documentElement.style.setProperty('--color-sidebar', state.colors.sideBarColor);
         document.documentElement.style.setProperty('--color-text', state.colors.textColor);
         document.documentElement.style.setProperty('--color-scroll-loader', state.colors.scrollLoaderColor);
 
-    }, [state.colors.sideBarColor, state.colors.cardColor, state.colors.textColor, state.colors.scrollLoaderColor])
+    }, [state.colors.sideBarColor, state.colors.cardColor, state.colors.textColor, state.colors.scrollLoaderColor, state.colors.bodyColor])
 
     useEffect(() => {
-        //document.body.style.background = state.colors.bodyColor;
-    }, [state.colors.bodyColor])
+        if (state.stateOfBackground === 'color') {
+            //document.documentElement.style.setProperty('--color-body', state.colors.bodyColor);
+            document.body.style.setProperty('background', `${state.colors.bodyColor}`)
+        } else {
+            const imageLink = state.images.activeImage;
+            document.body.style.setProperty('background-image', `url(${imageLink})`);
+            document.body.style.setProperty('background-position', 'center');
+            document.body.style.setProperty('background-repeat', 'repeat');
+            document.body.style.setProperty('background-size', 'cover');
+        }
+
+    }, [state.images.bodyImages, state.images.activeImage, state.colors.bodyColor, state.stateOfBackground])
 
     return (
         <GlassElement
@@ -118,58 +146,79 @@ const BrushSection: React.FC<BrushProps> = (props) => {
             <Accordion>
                 <Accordion.Item className="border-0" eventKey="0" style={{ 'background': 'var(--color-sidebar)' }}>
                     <Accordion.Header style={{ 'background': 'var(--color-sidebar)' }}>Select background</Accordion.Header>
-                    <Accordion.Body>
-                        {state.backgroundImages.map(imageURL => <img src={imageURL} alt="imgItem" className="w-25 h-25" />)}
-                        <div className="w-25 h-25" onClick={() => dispatchImmer({ type: 'updateStateOfBackground', value: 'color' })}></div>
+                    <Accordion.Body className='d-flex flex-wrap gap-1'>
+                        {state.images.bodyImages.map(imageURL => {
+                            return (
+                                <img
+                                    src={imageURL}
+                                    alt="imgItem"
+                                    className="p-1 w-25 h-25 cursor-pointer border"
+                                    key={imageURL}
+                                    id={imageURL}
+                                    onClick={() => {
+                                        dispatchImmer({ type: 'updateStateOfBackground', value: 'image' });
+                                        dispatchImmer({ type: 'updateActiveImage', value: imageURL })
+                                    }} />
+                            )
+                        })}
+                        <div
+                            className="w-25 p-1 border centered-content border cursor-pointer"
+                            onClick={() => dispatchImmer({ type: 'updateStateOfBackground', value: 'color' })}
+                        >
+                            <div className="w-100 h-100" style={{ 'background': 'var(--color-body)' }}></div>
+                        </div>
                         <RotateCard
                             classnames={classnamesOfRotateCard}
                             backFaceOfCard={
                                 <Form.Control
                                     className="w-75"
-                                    disabled={state.stateOfBackground === 'color'}
                                     type="file"
                                     name="image"
-                                    accept="image/png, image/jpeg"
-                                    // ref={ref}
-                                    // onChange={async () => {
-                                    //     const formData = new FormData();
-                                    //     const file = ref.current!.files[0];
-                                    //     formData.append(file.name, file)
-                                    //     const url = new URL('3/image', 'https://api.imgur.com/');
-                                    //     //url.searchParams.set('key', '55613fc8c4bcab8867f472a0998328e1')
-                                    //     const link = await axios(url.toString(), {
- 
-                                    //     });
-                                    //     console.log(link);
-                                    //     dispatchImmer({ type: 'updateStatebackgroundOfBody', value: link })
-                                    // }}
+                                    accept="image/png, image/jpeg, image/jpg"
+                                    ref={uploadFileRef}
+                                    onChange={async () => {
+                                        if (!uploadFileRef.current?.files) { return; }
+                                        const file = uploadFileRef.current.files[0];
+
+                                        const formData = new FormData();
+                                        formData.append('image', file)
+
+                                        const url = new URL('/1/upload', 'https://api.imgbb.com');
+                                        url.searchParams.set('key', '55613fc8c4bcab8867f472a0998328e1');
+                                        const { data: response } = await axios<IResponseData>({
+                                            method: 'post',
+                                            url: url.toString(),
+                                            data: formData,
+                                        });
+
+
+                                        dispatchImmer({ type: 'updateBodyImages', value: response.data.url })
+                                    }}
                                 />
                             }
                             frontFaceOfCard={
-                                <Form.Label>Select-bakcground-image</Form.Label>
+                                <Form.Label>Add-background-image</Form.Label>
+                            }
+                        />
+                        <RotateCard
+                            classnames={classnamesOfRotateCard}
+                            backFaceOfCard={
+                                <Form.Control
+                                    className="w-75"
+                                    type="color"
+                                    name="bodyColor"
+                                    value={state.colors.bodyColor}
+                                    onChange={(event) => dispatchImmer({ type: 'updateBodyColor', value: event.target.value })}
+                                    aria-label="select-color"
+                                />
+                            }
+                            frontFaceOfCard={
+                                <Form.Label>Select body color</Form.Label>
                             }
                         />
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
-
-            <RotateCard
-                classnames={classnamesOfRotateCard}
-                backFaceOfCard={
-                    <Form.Control
-                        className="w-75"
-                        disabled={state.stateOfBackground !== 'color'}
-                        type="color"
-                        name="color"
-                        value={state.colors.bodyColor}
-                        onChange={(event) => dispatchImmer({ type: 'updateBodyColor', value: event.target.value })}
-                        aria-label="select-color"
-                    />
-                }
-                frontFaceOfCard={
-                    <Form.Label>Select body color</Form.Label>
-                }
-            />
 
             <RotateCard
                 classnames={classnamesOfRotateCard}
--- src/helpersFunc/saveOptionsOfStyleHandler.ts
+++ src/helpersFunc/saveOptionsOfStyleHandler.ts
@@ -1,21 +1,30 @@
 import { IInitialStateImmer } from "../components/sidebar/brush/BrushSection";
 
 const saveOptionsOfStyleHandler = (state:IInitialStateImmer): void => {
-    const { 
+    const {
+        images: {
+            activeImage,
+            bodyImages
+        },
         colors: {
             bodyColor,
             sideBarColor,
+            cardColor,
             textColor,
             scrollLoaderColor,
         },
-        stateOfBackground 
+        stateOfBackground,
+
     } = state;
 
     localStorage.setItem('bodyColor', bodyColor);
     localStorage.setItem('sideBarColor', sideBarColor);
+    localStorage.setItem('cardColor', cardColor);
     localStorage.setItem('textColor', textColor);
     localStorage.setItem('scrollLoaderColor', scrollLoaderColor);
     localStorage.setItem('stateOfBackground', stateOfBackground);
+    localStorage.setItem('activeImage', activeImage);
+    localStorage.setItem('bodyImages', JSON.stringify(bodyImages));
 }
 
 export default saveOptionsOfStyleHandler;
